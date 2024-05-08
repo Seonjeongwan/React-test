2caa566729352195cae7c6c51d697b57ba4e9663
Seon <seon@Seonui-MacBookPro.local>
2024-05-08 11:20:27 +0700

    link tag 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main";
+import ProductPage from "./product";
+import UploadPage from "./upload";
 import TimerComponent from "./timer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPageComponent />} />
+      <Route path="/products/:id" element={<ProductPage />} />
+      <Route path="/upload" element={<UploadPage />} />
+    </Routes>
+  );
 }
 
 export default App;
