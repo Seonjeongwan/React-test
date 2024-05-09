e549567df270a6db46338734b3aed9f82910f394
Seon <seon@Seonui-MacBookPro.local>
2024-05-09 15:15:23 +0700

    antd 적용

--- src/App.js
+++ src/App.js
@@ -4,15 +4,45 @@ import MainPageComponent from "./main";
 import ProductPage from "./product";
 import UploadPage from "./upload";
 import TimerComponent from "./timer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <Routes>
-      <Route path="/" element={<MainPageComponent />} />
-      <Route path="/products/:id" element={<ProductPage />} />
-      <Route path="/upload" element={<UploadPage />} />
-    </Routes>
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              navigate("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Routes>
+          <Route path="/" element={<MainPageComponent />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </div>
+      <div id="footer"></div>
+    </div>
   );
 }
 
--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,45 @@
+import "./index.css";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://54ad73c7-016e-4b63-b760-801d385fbc3d.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  // console.log(product);
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다....</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={`/${product.imageUrl}`} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="content-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
